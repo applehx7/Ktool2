40bd78b584715b96244f4c354d6d02869cb9ff8d
Applehx Ducky <131701531+applehx7@users.noreply.github.com>
2023-04-26 13:22:31 +0600

    Delete d_add.sh

--- d_add.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "# Kali linux repositories | Added by Ktool2" >> /etc/apt/sources.list
-echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
-apt update
